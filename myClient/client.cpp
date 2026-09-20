068809d5611b4bc21e4ae188dad9afed192ae4df
Aamil Farooq <aamil.farooq@gmail.com>
2026-09-20 22:05:39 +0530

    Basic communication working with linux client

---
+++ myClient/client.cpp
@@ -0,0 +1,130 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #pragma comment(lib, "ws2_32.lib")
+    typedef SOCKET socket_t;
+    #define CLOSE_SOCKET(s) closesocket(s)
+    const socket_t INVALID_SOCKET_VALUE = INVALID_SOCKET;
+
+#else
+    #include <arpa/inet.h>
+    #include <netinet/in.h>
+    #include <sys/socket.h>
+    #include <unistd.h>
+    typedef int socket_t;
+    #define CLOSE_SOCKET(s) close(s)
+    const socket_t INVALID_SOCKET_VALUE = -1;
+
+#endif
+
+bool send_all(socket_t sock, const char* data, size_t length) 
+{
+    while (length > 0) 
+	{
+        int sent = send(sock, data, static_cast<int>(length), 0);
+
+        if (sent <= 0) {
+            return false;
+        }
+
+        data += sent;
+        length -= static_cast<size_t>(sent);
+    }
+
+    return true;
+}
+
+int main(int argc, char* argv[]) 
+{
+    //const char* server_ip = argc > 1 ? argv[1] : "127.0.0.1";
+    //unsigned short port = argc > 2 ? static_cast<unsigned short>(std::atoi(argv[2])) : 50005;
+    const char* server_ip = "172.18.114.232";
+    unsigned short port = 50005;
+
+#ifdef _WIN32
+    WSADATA wsa_data;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) 
+	{
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
+#endif
+
+    socket_t sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (sock == INVALID_SOCKET_VALUE) 
+	{
+        std::cerr << "Could not create socket\n";
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
+
+    sockaddr_in server_addr;
+    std::memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) != 1) 
+	{
+        std::cerr << "Invalid IPv4 address: " << server_ip << '\n';
+        CLOSE_SOCKET(sock);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
+
+    if (connect(sock, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr)) != 0) 
+	{
+        std::cerr << "Could not connect to " << server_ip << ":" << port << '\n';
+        CLOSE_SOCKET(sock);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
+
+    std::cout << "Connected to " << server_ip << ":" << port << '\n';
+    std::cout << "Enter text. Press Ctrl+D (Linux) or Ctrl+Z then Enter (Windows) to exit.\n";
+
+    std::string message;
+    char reply[256];
+
+    while (std::getline(std::cin, message)) 
+	{
+        message += '\n';
+
+        if (!send_all(sock, message.c_str(), message.size())) 
+		{
+            std::cerr << "Send failed\n";
+            break;
+        }
+
+        int received = recv(sock, reply, sizeof(reply) - 1, 0);
+
+        if (received <= 0) 
+		{
+            std::cout << "Server disconnected\n";
+            break;
+        }
+
+        reply[received] = '\0';
+        std::cout << "Server reply: " << reply;
+    }
+
+    CLOSE_SOCKET(sock);
+
+#ifdef _WIN32
+    WSACleanup();
+#endif
+
+    return 0;
+}
---
+++ myServer/server.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <cstring>
+
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #pragma comment(lib, "ws2_32.lib")
+    typedef SOCKET socket_t;
+    #define CLOSE_SOCKET(s) closesocket(s)
+#else
+    #include <arpa/inet.h>
+    #include <netinet/in.h>
+    #include <sys/socket.h>
+    #include <unistd.h>
+    typedef int socket_t;
+    #define CLOSE_SOCKET(s) close(s)
+#endif
+
+int main() 
+{
+#ifdef _WIN32
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) 
+	{
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
+#endif
+
+    const unsigned short port = 50005;
+    //const char* server_ip = "127.0.0.1";
+    const char* server_ip = "172.18.114.232";
+
+    socket_t server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) 
+	{
+        std::cerr << "socket() failed\n";
+        return 1;
+    }
+
+    int reuse = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char*>(&reuse), sizeof(reuse));
+
+    sockaddr_in address;
+    std::memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    //address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_addr.s_addr = inet_addr(server_ip);
+    address.sin_port = htons(port);
+
+    if (bind(server_fd, reinterpret_cast<sockaddr*>(&address), sizeof(address)) < 0) 
+	{
+        std::cerr << "bind() failed\n";
+        CLOSE_SOCKET(server_fd);
+        return 1;
+    }
+
+    if (listen(server_fd, 10) < 0) 
+	{
+        std::cerr << "listen() failed\n";
+        CLOSE_SOCKET(server_fd);
+        return 1;
+    }
+
+    std::cout << "Server listening on port " << port << '\n';
+
+    while (true) 
+	{
+        sockaddr_in client_address;
+        socklen_t client_size = sizeof(client_address);
+
+        socket_t client_fd = accept(
+            server_fd,
+            reinterpret_cast<sockaddr*>(&client_address),
+            &client_size
+        );
+
+        if (client_fd < 0) 
+		{
+            std::cerr << "accept() failed\n";
+            continue;
+        }
+
+        char client_ip[INET_ADDRSTRLEN] = {};
+        inet_ntop(AF_INET, &client_address.sin_addr, client_ip, sizeof(client_ip));
+
+        std::cout << "Client connected: " << client_ip << '\n';
+
+        char buffer[4096];
+        int received;
+
+        while ((received = recv(client_fd, buffer, sizeof(buffer) - 1, 0)) > 0) 
+		{
+            buffer[received] = '\0';
+
+            std::cout << "Received: " << buffer << '\n';
+
+            const char reply[] = "OK\n";
+            send(client_fd, reply, static_cast<int>(sizeof(reply) - 1), 0);
+        }
+
+        std::cout << "Client disconnected\n";
+        CLOSE_SOCKET(client_fd);
+    }
+
+    CLOSE_SOCKET(server_fd);
+
+#ifdef _WIN32
+    WSACleanup();
+#endif
+
+    return 0;
+}
